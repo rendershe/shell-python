fc582ef0848b3aae9e5f579f0af80589f47e4a15
rendershe <120629170+rendershe@users.noreply.github.com>
2022-12-15 00:20:54 -0800

    Update url.py

--- url.py
+++ url.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.db.models import F
 from django.db import IntegrityError, transaction
-
+import time
 from  common.models import  Order,OrderMedicine
 
 import json
