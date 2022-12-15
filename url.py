22e422aa774e0764a7c29b7e2a0649376d836f44
rendershe <120629170+rendershe@users.noreply.github.com>
2022-12-15 00:19:20 -0800

    Create url.py

---
+++ url.py
@@ -0,0 +1,43 @@
+from django.http import JsonResponse
+from django.db.models import F
+from django.db import IntegrityError, transaction
+
+from  common.models import  Order,OrderMedicine
+
+import json
+
+def dispatcher(request):
+
+    if 'usertype' not in request.session:
+        return JsonResponse({
+            'ret': 302,
+            'msg': 'unlogged',
+            'redirect': '/mgr/sign.html'},
+            status=302)
+
+    if request.session['usertype'] != 'mgr':
+        return JsonResponse({
+            'ret': 302,
+            'msg': 'User non-mgr type',
+            'redirect': '/mgr/sign.html'},
+            status=302)
+
+
+    if request.method == 'GET':
+        request.params = request.GET
+
+    elif request.method in ['POST','PUT','DELETE']:
+
+        request.params = json.loads(request.body)
+
+
+    action = request.params['action']
+    if action == 'list_order':
+        return listorder(request)
+    elif action == 'add_order':
+        return addorder(request)
+
+
+
+    else:
+        return JsonResponse({'ret': 1, 'msg': 'No class type HTTP request'})
